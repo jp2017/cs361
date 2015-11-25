0ae9cc5458ba72d3706f9d14455a11ebdfa30bef
jp2017 <phamju@oregonstate.edu>
2015-11-24 19:41:45 -0800

    Added a bit more information

--- db.sql
+++ db.sql
@@ -1,6 +1,33 @@
+DROP TABLE charities;
+DROP TABLE services;
+
 CREATE TABLE charities
 (
-charityname varchar(50),
-description varchar(200),
-location varchar(50)
+charityname varchar(100),
+description varchar(500),
+location varchar(50),
+services varchar(50),
+volunteer varchar(50)
 );
+
+CREATE TABLE services
+(
+charityname varchar(100),
+description varchar(500)
+);
+
+INSERT INTO `charities`(`charityname`, `description`, `location`, `services`, `volunteer`) VALUES ('Oregon Heat','Oregon HEAT is an independent nonprofit organization founded in 1989 as Oregon Energy Services to help low-income Oregonians become more energy self-reliant','Tualatin', 'services', 'volunteer');
+
+INSERT INTO `charities`(`charityname`, `description`, `location`,`services`,`volunteer`) VALUES ('Oregon Food Bank','Oregon Food Bank works to eliminate hunger and its root causes ... because no one should be hungry. Oregon Food Bank recovers food from farmers, manufacturers, wholesalers, retailers, individuals and government sources.','Portland', 'services', 'volunteer');
+
+INSERT INTO `charities`(`charityname`, `description`, `location`,`services`,`volunteer`) VALUES ('Metropolitan Affordable Housing Corp.','Metropolitan Affordable Housing Corporation is an entrepreneurial non-profit organization committed to improving the quality of life for people living on limited incomes. We develop and manage quality affordable housing communities','Eugene', 'services', 'volunteer');
+
+INSERT INTO services(`charityname`, `description`) VALUES ('Oregon Heat', 'Providing funds to low incomes families or individuals');
+
+INSERT INTO services(`charityname`, `description`) VALUES ('Oregon Food Bank', 'Providing food');
+
+INSERT INTO services(`charityname`, `description`) VALUES ('Metropolitan Affordable Housing Corp.', 'Providing affordable housing');
+
+
+
+
--- topbar.php
+++ topbar.php
@@ -1,5 +1,7 @@
-<p class = "topbar">  
-    <?php
-        echo "test header1\n";
-    ?>
-</p>
+<div class="navigation" id="topbar">
+  <form name="topbar" method="post" action="">
+      <input type="Submit" name="My Account" id="My Account" value="My Account">
+      <input type="Submit" name="Donate" id="Donate" value="Donate Now">
+      <input type="Submit" name="Donate" id="Donate" value="Contact Us">
+  </form>
+</div>
