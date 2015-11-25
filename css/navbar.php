847f29b5cc0dcce9537707f6a4119b32a18447b0
jp2017 <phamju@oregonstate.edu>
2015-11-25 15:44:42 -0800

    fixing some of the old code
    fixed code

---
+++ css/navbar.php
@@ -0,0 +1,28 @@
+
+<div class="headerBar" id="headerBar">
+    <TABLE BORDER="0">
+    <TR>
+    
+    <td class="navtitletd">
+        <p class="toptitle">Second Chance Association</p>
+    </td>
+    <form name="topbar" method="post" action="">
+    <td>
+        <input type="Submit" name="My Account" id="My Account" value="My Account" class="myButton">
+    </td>  
+    <td>
+        <input type="Submit" name="Donate" id="Donate" value="Donate Now" class="myButton">
+    </td> 
+    <td>
+        <input type="Submit" name="Donate" id="Donate" value="Contact Us" class="myButton">
+    </td> 
+    </form>
+
+    </TR>
+    </TABLE>
+    
+</div>
+
+
+
+
