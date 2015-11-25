670f7febed4d6e162dfe13471c4ef42394de90ae
jp2017 <phamju@oregonstate.edu>
2015-11-24 20:18:18 -0800

    Added a bit more information
    testing

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
 <!DOCTYPE html>
 <html>
-    
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>GETTING STARTED WITH BRACKETS</title>
-        <meta name="description" content="An interactive getting started guide for Brackets.">
+        <title>Second Chance Association</title>
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+        <meta http-equiv="refresh" content="60" />
         <link rel="stylesheet" href="/css/style.css">
     </head>
     <? include 'topbar.php'?>
     <body>
-        <? include 'connectiontest.php' ?>
+        <div id="page">
+            <div id="content">
+                <div><h3>Welcome to Second Chance Assiociation</h3></div>
+                <p>
+                    Second Chance Assiociation is here to serve those who took a wrong path and now are in needed for a second chance in life. We take a great pride in helping everyone live better lives.   
+                </p>
+                <? include 'connectiontest.php' ?>
+            </div>
+        </div>
     </body>
 </html>
